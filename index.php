1876ad1a39e47f374df0fb2bb76c4bc0ccfe6f5c
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-11-29 15:41:05 +0100

    tentativo creazione partials function

--- index.php
+++ index.php
@@ -1,47 +1,54 @@
 <?php
+session_start();
+include __DIR__ . '/partials/header.php';
+include __DIR__ . '/partials/functions.php';
+
+$caratteri = [
+    'alphabet' => 'qwertyuiopasdfghjklzxcvbnm',
+    'numbers' => '1234567890',
+    'symbols' => '\|!"£$%&/()=?^*.;,-_#@][><'
+];
+getCharacter($string);
+
+
+$password = '';
+if (isset($_GET['lunghezza']) && !empty($_GET['lunghezza'])) {
+    $lunghezza = $_GET['lunghezza'];
+    while (strlen($password) < $lunghezza) {
+        $password .= getCharacter($caratteri['alphabet']);
+    }
+}
+;
+
 
 
 ?>
 
-<!DOCTYPE html>
-<html lang='en'>
 
-<head>
-    <meta charset='UTF-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-        crossorigin="anonymous"></script>
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
-        integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=='
-        crossorigin='anonymous' referrerpolicy='no-referrer' />
-    <link rel='stylesheet' href='./CSS/style.css'>
 
-    <script src='./JS/script.js' defer></script>
-    <title>Document</title>
-</head>
+
 
 <body>
+    <main>
+        <form action="index.php" method="GET">
 
-    <form action="index.php" method="GET">
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Lunghezza</label>
+                <input type="number" class="form-control" name="lunghezza">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">genera</button>
+            <button type="refer" class="btn btn-primary">annulla</button>
+        </form>
+
+        <div>
+            <?php echo $password . strlen($password) ?>
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Genera</button>
-        <button type="reset" class="btn btn-primary">Annulla</button>
-    </form>
+
+
+    </main>
 
 </body>
---
+++ partials/functions.php
@@ -0,0 +1,7 @@
+<?php
+function getCharacter($string): string
+{
+    $lunghezza = strlen($string) - 1;
+    $randomNumber = rand(0, $lunghezza);
+    return $string[$randomNumber];
+};
